58c3d775579c2c4961de29375db8771fecd0c0ac
Mateus Borges <mateusaraujoborges@gmail.com>
2018-03-20 18:48:47 +0000

    minor clean-up

--- regression_tests/test_es6.py
+++ regression_tests/test_es6.py
@@ -4,13 +4,14 @@ def test_ec6():
     counters = {}
     path_name = os.path.join(constants.seeds_dir, 'WebKit.JSTests.es6')
     with open('diff_report_test_ec6.txt', 'w') as file:
-        buffer = ''
+        buffer = []
         for file_name in os.listdir(path_name):
             res = multicall.callAll(os.path.join(path_name, file_name))
             # discrepancy if we find diverging responses
             if res.should_report():
                 res.update_counters(counters)
-                buffer += res.__str__()           
+                buffer.append(str(res))           
         file.write('**** summary ****\n')
-        file.write(counters.__str__()+"\n")
-        file.write(buffer)
+        file.write(counters)
+        file.write("\n")
+        file.write(''.join(buffer))
