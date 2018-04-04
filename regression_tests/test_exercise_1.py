87a18bf4bd804eacbf3637fe7f81679e2cb9dec3
pag-tools <euninguem90@gmail.com>
2018-04-04 15:15:54 -0300

    exercise: reproducing a bug reported

---
+++ regression_tests/test_exercise_1.py
@@ -0,0 +1,7 @@
+from regression_tests import *
+from utils import constants, multicall
+from fuzzer.validator import validate
+
+def test_exercise_1():
+    path_name = os.path.join(constants.seeds_dir, 'exercise_1')
+    multicall.multicall_directories(path_name, False, validator=validate)
---
+++ seeds/exercise_1/chakra_4919.js
@@ -0,0 +1,4 @@
+// Found by Igor Simoes
+// https://github.com/Microsoft/ChakraCore/issues/4919
+
+let chakra = (o, {name = 'foo'}) => {};
