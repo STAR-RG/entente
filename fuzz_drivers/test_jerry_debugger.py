773fbe0a7bacf5f993eab2c4483a195a0089cde0
Bruna Cruz <baac@cin.ufpe.br>
2018-05-11 00:44:08 -0300

    correcao

--- fuzz_drivers/test_jerry_debugger.py
+++ fuzz_drivers/test_jerry_debugger.py
@@ -9,7 +9,7 @@ def test_fuzz_jerry_debugger():
     path_name = os.path.join(constants.seeds_dir, 'JerryJS/debugger/baac')
     multicall.multicall_directories(path_name, True, validator=validate)
     
-                                    
+@pytest.mark.skip(reason="temporarilly disabling")                                
 def test_fuzz_jerry_shell():
     path_name = os.path.join(constants.seeds_dir, 'JerryJS/debugger/baac/shell_files')
     multicall.multicall_directories(path_name, True, validator=validate),
