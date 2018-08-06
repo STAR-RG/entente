fcc69c58bc792342c1b41bc3bb2870bc46125652
igor-simoes <isol2@cin.ufpe.br>
2018-08-06 18:00:55 -0300

    updated piechart

--- paper/R/piecharts/falsepositives.r
+++ paper/R/piecharts/falsepositives.r
@@ -1,6 +1,6 @@
 # Create Data
-false_positive_perc=c(10,30,20,14,25)
-false_positive_names=c("Not Implemented (10)","Timeout/OME (30)", "Undefined (20)", "Future (14)", "Error Message Mismatch (25)")
+false_positive_perc=c(1,2,1,0,5)
+false_positive_names=c("Not Implemented (1)","Timeout/OME (2)", "Undefined (1)", "Future (0)", "Error Message Mismatch (5)")
 pie(false_positive_perc, labels=false_positive_names, radius=.5, cex=1.3)
 
 
