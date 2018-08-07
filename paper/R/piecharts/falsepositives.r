1c87fdf464ddc603fa5eb38359b0226227e12fb8
igor-simoes <isol2@cin.ufpe.br>
2018-08-07 09:29:13 -0300

    update values

--- paper/R/piecharts/falsepositives.r
+++ paper/R/piecharts/falsepositives.r
@@ -1,6 +1,5 @@
 # Create Data
-false_positive_perc=c(1,2,1,0,5)
-false_positive_names=c("Not Implemented (1)","Timeout/OME (2)", "Undefined (1)", "Future (0)", "Error Message Mismatch (5)")
-pie(false_positive_perc, labels=false_positive_names, radius=.5, cex=1.3)
-
-
+false_positive_perc=c(0,2,0,0,5,2)
+false_positive_names=c("Not Implemented","Timeout/OME", "Undefined", "Future", "Error Message Mismatch", "Invalid Test case")
+labs <- paste(false_positive_names, " (", false_positive_perc, ")", sep="")
+pie(false_positive_perc, labels=labs, radius=.5, cex=1.3)
--- paper/R/piecharts/truepositives.r
+++ paper/R/piecharts/truepositives.r
@@ -1,4 +1,7 @@
 # Create Data
-true_positive_perc=c(10,80)
-true_positive_names=c("Bug (10)","Duplicate (73)")
-pie(true_positive_perc , labels=true_positive_names, radius=.5, cex=1.2)
+true_positive_perc=c(5,1)
+true_positive_names=c("Bug","Duplicate")
+
+labs <- paste(true_positive_names, " (", true_positive_perc, ")", sep="")
+
+pie(true_positive_perc , labels=labs, radius=.5, cex=1.2)
