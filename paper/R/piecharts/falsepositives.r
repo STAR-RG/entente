13131237f20b1ef1ce7333fd23c2043cb1d786c4
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2018-08-07 16:21:12 -0300

    pass on V.B

--- paper/R/piecharts/falsepositives.r
+++ paper/R/piecharts/falsepositives.r
@@ -3,7 +3,7 @@
 # jsc 14
 # sm
 # ch
-false_positive_perc=c(1,3,2,0,5,3)
-false_positive_names=c("Not Implemented","Timeout/OME", "Undefined", "Future", "Error Message Mismatch", "Invalid Test case")
+false_positive_perc=c(1,3,9,1)
+false_positive_names=c("Unknown","Timeout/OME", "Undefined", "Not Implemented")
 labs <- paste(false_positive_names, " (", false_positive_perc, ")", sep="")
 pie(false_positive_perc, labels=labs, radius=.5, cex=1.3)
