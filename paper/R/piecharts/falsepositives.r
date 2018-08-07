1957dbd9dcbd39ed11a2f205164ec23115dcbe27
igor-simoes <isol2@cin.ufpe.br>
2018-08-07 17:55:28 -0300

    updated false positives

--- paper/R/piecharts/falsepositives.r
+++ paper/R/piecharts/falsepositives.r
@@ -1,9 +1,5 @@
 # Create Data
-# v8
-# jsc 14
-# sm
-# ch
-false_positive_perc=c(1,3,9,1)
+false_positive_perc=c(4,3,9,2)
 false_positive_names=c("Unknown","Timeout/OME", "Undefined", "Not Implemented")
 labs <- paste(false_positive_names, " (", false_positive_perc, ")", sep="")
 pie(false_positive_perc, labels=labs, radius=.5, cex=1.3)
