53a60c04776d4cd35d0c13d302437daf17976847
igor-simoes <isol2@cin.ufpe.br>
2018-08-16 17:43:13 -0300

    update results

--- paper/R/piecharts/falsepositives.r
+++ paper/R/piecharts/falsepositives.r
@@ -1,5 +1,5 @@
 # Create Data
-false_positive_perc=c(9,3,9,5)
+false_positive_perc=c(34,7,31,7)
 false_positive_names=c("Unknown","Timeout/OME", "Undefined", "Not Implemented")
 labs <- paste(false_positive_names, " (", false_positive_perc, ")", sep="")
 pie(false_positive_perc, labels=labs, radius=.5, cex=1.3)
--- paper/R/piecharts/truepositives.r
+++ paper/R/piecharts/truepositives.r
@@ -3,7 +3,7 @@
 # v8 new 1 dup 0
 # jsc new 5 dup 0
 # sm new 0 dup 2
-true_positive_perc=c(8,3)
+true_positive_perc=c(9,2)
 true_positive_names=c("Bug","Duplicate")
 
 labs <- paste(true_positive_names, " (", true_positive_perc, ")", sep="")
