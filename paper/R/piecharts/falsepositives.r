cbcf65c8df9c2677aab8e7001a4caa01ed332b35
igor-simoes <isol2@cin.ufpe.br>
2018-08-23 17:16:45 -0300

    pass on results

--- paper/R/piecharts/falsepositives.r
+++ paper/R/piecharts/falsepositives.r
@@ -1,5 +1,5 @@
 # Create Data
-false_positive_perc=c(34,7,31,7)
+false_positive_perc=c(132,13,313,21)
 false_positive_names=c("Unknown","Timeout/OME", "Undefined", "Not Implemented")
 labs <- paste(false_positive_names, " (", false_positive_perc, ")", sep="")
-pie(false_positive_perc, labels=labs, radius=.5, cex=1.3)
+pie(false_positive_perc, labels=labs, radius=.5, cex=1.1)
