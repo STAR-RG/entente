fd15d605fcff409178bda4d84763ab526cd30d23
Jefferson Junior <junior.jefferson@gmail.com>
2020-06-09 01:17:31 -0300

    Fig. 1 de acordo com novo experimento

--- paper/R/stackedbar/stacked-engine.r
+++ paper/R/stackedbar/stacked-engine.r
@@ -4,20 +4,23 @@ table <- read.table("/home/jefferson/Documentos/PPGCC UFPA/PESQUISA (Prof. Gusta
 counts <- table(table$Status, table$Engine)
 
 vector <- c(counts["2-Confirmed","Chakra"],
-            counts["2-Confirmed","JavaScriptCore"],
+            counts["2-Confirmed","JSC"],
             counts["2-Confirmed","V8"],
             counts["2-Confirmed","Hermes"],
+            counts["2-Confirmed","SpiderMonkey"],
             counts["3-Fixed","Chakra"],
-            counts["3-Fixed","JavaScriptCore"],
+            counts["3-Fixed","JSC"],
             counts["3-Fixed","V8"],
             counts["3-Fixed","Hermes"],
+            counts["3-Fixed","SpiderMonkey"],
             counts["1-New","Chakra"],
-            counts["1-New","JavaScriptCore"],
+            counts["1-New","JSC"],
             counts["1-New","V8"],
-            counts["1-New","Hermes"])
+            counts["1-New","Hermes"], 
+            counts["1-New","SpiderMonkey"])
 
-df <- data.frame(status=rep(c("2-Confirmed", "3-Fixed", "1-New"), each=4),
-                 engine=rep(c("Chakra", "JavaScriptCore", "V8", "Hermes"), each=1),
+df <- data.frame(status=rep(c("2-Confirmed", "3-Fixed", "1-New"), each=5),
+                 engine=rep(c("Chakra", "JSC", "V8", "Hermes", "SpiderMonkey"), each=1),
                  number=vector)
 
 ggplot(data=df, aes(x=engine, y=number, fill=status)) +
