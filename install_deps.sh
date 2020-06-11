a5e0f9ab6a10a59e4e8575a6138c8dbd06e3d655
Gustavo Pinto <mail@gustavopinto.org>
2020-06-11 17:40:23 -0300

    fixing new dir names in install_deps.sh script

--- install_deps.sh
+++ install_deps.sh
@@ -21,11 +21,11 @@ sudo npm install -g eshost-cli
 # curl -sSL https://get.haskellstack.org/ | sh
 # git clone https://github.com/igor-simoes/QuickFuzz.git --depth 1 && cd QuickFuzz && stack setup && stack install alex happy
 # by zip file in js_engines directory
-unzip js_engines/QuickFuzz.zip -d js_engines/
+unzip bin_fuzzers/QuickFuzz.zip -d bin_fuzzers/
 
 # configures jsvu and fuzzers
 PROJECT_DIR=`pwd`
-FUZZERS_DIR=$PROJECT_DIR/jsengines-differential-testing/js_engines/
+FUZZERS_DIR=$PROJECT_DIR/entente/_/
 echo 'export PATH="${PATH}:${HOME}/.jsvu:${FUZZERS_DIR}"' >> ~/.bashrc && source ~/.bashrc
 jsvu --os=linux64 --engines=chakra,javascriptcore,spidermonkey,v8,hermes
 eshost --configure-jsvu
